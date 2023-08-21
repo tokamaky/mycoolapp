b89868ff663db47845393097cd75d8add389694b
tokamaky <jxh186045@gmail.com>
2023-08-21 15:37:56 -0400

    Define custom properties

--- src/main/java/com/JiCode/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/JiCode/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,13 +1,26 @@
 package com.JiCode.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    // expose "/" that return "Hello World"
+    //inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
 
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public  String getTramInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }   
+
+    // expose "/" that return "Hello World"
     @GetMapping("/")
     public  String sayHello(){
         return "Hello World";
